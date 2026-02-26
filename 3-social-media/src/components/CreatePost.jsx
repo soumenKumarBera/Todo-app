a1a3dd035ab1fc546af2ac81d1ccc9970ed6be40
soumen kumar bera <soumenbera4017@gmail.com>
2026-02-26 15:20:59 +0530

    current value get in useRef

--- 3-social-media/src/components/CreatePost.jsx
+++ 3-social-media/src/components/CreatePost.jsx
@@ -1,28 +1,65 @@
+import { useContext, useRef } from "react";
+import {PostList} from "../store/post-list-store"
+
 const CreatePost = () => {
-
-
+//  const {addPost} = useContext(PostList);
 
   
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionElement = useRef();
+  const tagsElement = useRef();
+
+  const handelOnSubmit = (event) => {
+    event.preventDefault();
+    // current value get in useRef;
+    
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reaction = reactionElement.current.value;
+    const tags = tagsElement.current.value;
+
+      
+  };
+
+ 
+
   return (
-    <form className="create-post">
+    <form className="create-post" onSubmit={handelOnSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your userId
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          className="form-control"
+          id="userId"
+          placeholder="your user id..."
+        />
+      </div>
       <div className="mb-3">
         <label htmlFor="title" className="form-label">
           Post Title
         </label>
         <input
           type="text"
+          ref={postTitleElement}
           className="form-control"
           id="title"
           placeholder="How are you feeling today..."
         />
       </div>
 
-       <div className="mb-3">
+      <div className="mb-3">
         <label htmlFor="body" className="form-label">
           Post content
         </label>
         <textarea
           type="text"
+          ref={postBodyElement}
           rows="4"
           className="form-control"
           id="body"
@@ -30,35 +67,25 @@ const CreatePost = () => {
         />
       </div>
 
-       <div className="mb-3">
-        <label htmlFor="userId" className="form-label">
-          Enter your userId
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="userId"
-          placeholder="your user id..."
-        />
-      </div>
-
-       <div className="mb-3">
+      <div className="mb-3">
         <label htmlFor="reaction" className="form-label">
           number of reaction
         </label>
         <input
           type="text"
+          ref={reactionElement}
           className="form-control"
           id="reaction"
           placeholder="Howmany people react this post"
         />
       </div>
-       <div className="mb-3">
+      <div className="mb-3">
         <label htmlFor="tags" className="form-label">
           Enter your hasTags here
         </label>
         <input
           type="text"
+          ref={tagsElement}
           className="form-control"
           id="tags"
           placeholder="Plese enter tags using space"
