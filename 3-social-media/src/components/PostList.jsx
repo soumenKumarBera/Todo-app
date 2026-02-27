16eb45a944fa4abc30199f5cbead1c64237db647
soumen kumar bera <soumenbera4017@gmail.com>
2026-02-27 14:00:52 +0530

    post list empty messege

--- 3-social-media/src/components/PostList.jsx
+++ 3-social-media/src/components/PostList.jsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as postListData } from "../store/post-list-store";
+import WelcomeMsg from "./welecomeMsg";
 const PostList = () => {
   const { postList } = useContext(postListData);
   console.log(postList);
 
   return (
     <>
+      {postList.length === 0 && <WelcomeMsg />}
       {postList.map((post) => (
-        <Post key={post.id} post = {post}/>
+        <Post key={post.id} post={post} />
       ))}
     </>
   );
---
+++ 3-social-media/src/components/welecomeMsg.jsx
@@ -0,0 +1,10 @@
+const WelcomeMsg = () => {
+  return (
+    <center>
+     
+      <h1 className="welcomeMsg">There are no post</h1>{" "}
+    </center>
+  );
+};
+
+export default WelcomeMsg;
