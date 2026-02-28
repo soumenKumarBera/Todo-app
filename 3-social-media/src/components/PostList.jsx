cc30734683420a0b00e9f9d422c1fff7996d58d4
soumen kumar bera <soumenbera4017@gmail.com>
2026-02-28 09:35:45 +0530

    handing loading state

--- 3-social-media/src/components/PostList.jsx
+++ 3-social-media/src/components/PostList.jsx
@@ -1,25 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as postListData } from "../store/post-list-store";
 import WelcomeMsg from "./welecomeMsg";
+import LodingSpiner from "./lodingSpiner";
 const PostList = () => {
   const { postList, addInitialPost } = useContext(postListData);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((obj) => {
         console.log(obj.posts);
         addInitialPost(obj.posts);
+        setFetching(false);
       });
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMsg />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {" "}
+      {fetching && <LodingSpiner />}
+      {!fetching && postList.length === 0 && <WelcomeMsg />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- 3-social-media/src/components/welecomeMsg.jsx
+++ 3-social-media/src/components/welecomeMsg.jsx
@@ -2,9 +2,6 @@ const WelcomeMsg = () => {
   return (
     <center>
       <h1 className="welcomeMsg">There are no post</h1>
-      <button type="button" class="btn btn-primary">
-        Get Post From Server
-      </button>
     </center>
   );
 };
